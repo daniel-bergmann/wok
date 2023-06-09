3748fa19f67bd5dc9c85ac6fc82bdfa09ef581ef
daniel-bergmann <danielbergmann@ok.is>
2023-06-09 22:04:25 +0000

    env var fix

--- app/components/Search.tsx
+++ app/components/Search.tsx
@@ -1,8 +1,10 @@
 "use client"
+import Image from "next/image"
 import React, { useState } from "react"
 
 const Search = () => {
   const [searchQuery, setSearchQuery] = useState("")
+  const [results, setResults] = useState({ results: [] })
 
   const handleKeyPress = (event: any) => {
     if (event.key === "Enter") {
@@ -13,10 +15,13 @@ const Search = () => {
   const fetchData = () => {
     // Perform the fetch request with the search query
     // Example:
-    fetch(`${process.env.NEXT_PUBLIC_COMPLEX}${searchQuery}`)
+    fetch(
+      `${process.env.NEXT_PUBLIC_RECIPE}complexSearch${process.env.NEXT_PUBLIC_KEY}&query=${searchQuery}`
+    )
       .then((response) => response.json())
       .then((data) => {
         // Process the fetched data
+        setResults(data)
         console.log(data)
       })
       .catch((error) => {
@@ -34,6 +39,18 @@ const Search = () => {
         onKeyPress={handleKeyPress}
       />
       <button onClick={fetchData}>Search</button>
+
+      {results.results.map((recipe: any) => (
+        <div key={recipe.id}>
+          <h2>{recipe.title}</h2>
+          <Image
+            alt={recipe.title}
+            width={100}
+            height={100}
+            src={recipe.image}
+          />
+        </div>
+      ))}
     </div>
   )
 }
