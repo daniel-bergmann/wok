e24483f6186b1d2398138475e02c0f4eaaf9bd7e
daniel-bergmann <danielbergmann@ok.is>
2023-06-10 14:18:02 +0000

    added some styling

--- app/recipes/[recipe]/page.tsx
+++ app/recipes/[recipe]/page.tsx
@@ -1,5 +1,7 @@
 import React from "react"
 import Image from "next/image"
+import { MdOutlineRamenDining } from "react-icons/md"
+import { RxLapTimer } from "react-icons/rx"
 
 async function getRecipeDetails(id: number) {
   const response = await fetch(
@@ -20,28 +22,50 @@ async function getIngredients(id: number) {
 export default async function RecipePage({ params }: any) {
   const ingredients = await getIngredients(params.recipe)
   const details = await getRecipeDetails(params.recipe)
-  console.log(details)
   return (
-    <div>
-      <h1>{details.title}</h1>
-      <p>cuisines: {details.cuisines}</p>
-      <p>{details.diets}</p>
-      <Image alt={details.title} width={100} height={100} src={details.image} />
-      <p>Time to cook {details.readyInMinutes}</p>
-      {ingredients.ingredients.map((ingredient: any) => {
-        return (
-          <div>
-            <h3>{ingredient.name}</h3>
-            <p>
-              {ingredient.amount.metric.value} {ingredient.amount.metric.unit}
-            </p>
-          </div>
-        )
-      })}
-      {/* dangerously */}
-      <div dangerouslySetInnerHTML={{ __html: details.instructions }} />
-      <h4> Wines to pair {details.winePairing.pairedWines}</h4>
-      <p>{details.winePairing.pairingText}</p>
-    </div>
+    <section className="recipe-container">
+      <div className="row head">
+        <h1>{details.title}</h1>
+        <Image
+          alt={details.title}
+          width={200}
+          height={150}
+          src={details.image}
+        />
+      </div>
+      <div className="row time">
+        <div>
+          <RxLapTimer />
+          <p>{details.readyInMinutes} min</p>
+        </div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto
+          accusantium repellat hic.
+        </p>
+      </div>
+
+      <div className="row ingredients">
+        <div className="heading">
+          <h3>ingredients</h3>
+          <MdOutlineRamenDining />
+        </div>
+        <div className="ingredient-list">
+          {ingredients.ingredients.map((ingredient: any) => {
+            return (
+              <>
+                <p>
+                  {ingredient.amount.metric.value}
+                  {ingredient.amount.metric.unit} ◌ {ingredient.name}
+                </p>
+              </>
+            )
+          })}
+        </div>
+      </div>
+      <div
+        className="row"
+        dangerouslySetInnerHTML={{ __html: details.instructions }}
+      />
+    </section>
   )
 }
